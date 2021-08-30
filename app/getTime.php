f409d4f890515e79d10b0cf11525aa16abdb0abf
Maciej Krysa <Maciej.Krysa@pekao.com.pl>
2021-08-30 16:46:00 +0200

    add currentDate

--- app/getTime.php
+++ app/getTime.php
@@ -1,6 +1,7 @@
 <?php
 error_reporting(0);
 date_default_timezone_set("Europe/Warsaw");
+$resp->currentDate=date("Y:m:d");
 $resp->currentTime=date("H:i:s");
 header('Content-type: application/json');
 echo json_encode( $resp );
